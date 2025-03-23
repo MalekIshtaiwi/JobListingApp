31c218cb7db2aa8b794210fbff7af84a36bbd4e9
Malik Ishtaiwe <malekishtiwi3@gmail.com>
2025-03-23 07:16:54 +0300

    started using validation but it's still not working

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -19,7 +19,7 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="title" id="title"
                                     class="block min-w-0 grow py-1.5  px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="janesmith">
+                                    placeholder="janesmith" >
                             </div>
                         </div>
                     </div>
@@ -30,12 +30,19 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="salary" id="salary"
                                     class="block min-w-0 grow py-1.5  px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="janesmith">
+                                    placeholder="janesmith" >
                             </div>
                         </div>
                     </div>
 
                 </div>
+                @if ($errors->any())
+                    <ul>
+                        @foreach ($errors as $error )
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </div>
 
 
